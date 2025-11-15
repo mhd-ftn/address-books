6aef547accb3c6c5d4f1bb2a58f8dfd48ef9a4ff
mhd-ftn <lambeng241@gmail.com>
2025-11-15 11:20:13 +0700

    feat : create contact variable

---
+++ script.js
@@ -0,0 +1,14 @@
+const contact = [
+    {
+        id: 1,
+        Nama: "Syifa", 
+        NoTelp: "051424858414",
+    },
+    {
+        id:2,
+        Nama: "Cinta",
+        NoTelp: "08516451",
+    },
+];
+
+console.log(contact);
